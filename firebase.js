ab8b018d9173ae5e576928e8ad4ab553e6066a05
sauflex <flexsau@gmail.com>
2025-07-13 20:40:47 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,1 +1,11 @@
-// Firebase config placeholder, insert your keys here
+const firebaseConfig = {
+  apiKey: "AIzaSyBeuoKAch-e0VzJo16fRtLAvYJ5USd5jvY",
+  authDomain: "sauflex-orders-310a2.firebaseapp.com",
+  projectId: "sauflex-orders-310a2",
+  storageBucket: "sauflex-orders-310a2.appspot.com",
+  messagingSenderId: "334646608528",
+  appId: "1:334646608528:web:3d8b82ed96a47d12a9bd5"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
